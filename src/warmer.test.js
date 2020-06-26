6c985ccd83065219ad13d35f7e930612a4611a65
alberto <alberto.websci@gmail.com>
2020-06-26 12:23:12 +0200

    finished testing

--- src/warmer.test.js
+++ src/warmer.test.js
@@ -1,5 +1,26 @@
 /* eslint-env jest */
 
-test('check that mongo is working correctly', () => {
+const config = {
+  pages: 2,
+  startDate: '2010-01-01',
+  mongoUrl: 'mongodb://localhost:27017',
+  dbName: 'gitstars',
+  collectionName: 'gitstars',
+  sslCrt: 'certs/example.crt',
+  sslKey: 'certs/example.key'
+}
+const mongoDB = require('mongodb')
 
+const mongoClient = mongoDB.MongoClient
+
+test('check that mongo is working correctly', async () => {
+  let client
+
+  try {
+    client = await mongoClient.connect(config.mongoUrl, { useUnifiedTopology: true })
+    expect(client).toBeTruthy()
+    client.close()
+  } catch (err) {
+    throw new Error(err)
+  }
 })
